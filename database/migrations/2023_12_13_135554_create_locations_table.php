956956399233680eae1ffdc75450092b1f13f5d7
Aldino Kelvin <146757421+kelvxn@users.noreply.github.com>
2023-12-16 22:26:55 +0700

    update

--- database/migrations/2023_12_13_135554_create_locations_table.php
+++ database/migrations/2023_12_13_135554_create_locations_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('city');
+            $table->string('district');
         });
     }
 
--- database/migrations/2023_12_13_135555_create_products_table.php
+++ database/migrations/2023_12_13_135555_create_products_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('type_room_id');
+            $table->foreign('type_room_id')->references('id')->on('type_rooms');
+            $table->unsignedBigInteger('location_id');
+            $table->foreign('location_id')->references('id')->on('locations');
+            $table->string('name');
+            $table->string('size');
+            $table->string('locations');
+            $table->bigInteger('price');
+            $table->enum("electricity", [0,1]);
+            $table->enum("water", [0,1]);
+            $table->enum("wifi", [0,1]);
+            $table->enum("ac", [0,1]);
+            $table->enum("kitchen", [0,1]);
+            $table->enum("laundry", [0,1]);
+            $table->string('descs');
             $table->timestamps();
         });
     }
